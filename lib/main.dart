9f36220c08d81eb551858388d3c6654b81a4a8df
Rikza Kurnia <rikza.kurnia@ui.ac.id>
2022-11-17 15:05:21 +0700

    finish tutorial 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:pbp_app/form.dart';
+import 'package:pbp_app/page/form.dart';
 import 'package:flutter/material.dart';
+import 'package:pbp_app/page/to_do_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -70,6 +71,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+            title: const Text('To Do'),
+            onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
